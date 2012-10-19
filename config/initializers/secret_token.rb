185e40cfcb6b5a19b9a9c1156736815858eeff2a
John Figueiredo <jfigueir33@gmail.com>
2012-10-19 01:50:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserMailer::Application.config.secret_token = '5276b54f4079d3e6e7f73e8d60cece8fddaa89880f6dc2132a0998ac96103a5dcbc40267d3d7e9221a091df881bf9d90819d8286941eca6e3ea0a0029b425530'
